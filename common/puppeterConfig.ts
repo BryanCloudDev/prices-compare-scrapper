c61f2fc42da0d442a9dd3bfe66fac970e592efe3
Bryan Portillo <bportillo701@gmail.com>
2023-11-19 02:02:33 -0600

    removing pop up window

--- common/puppeterConfig.ts
+++ common/puppeterConfig.ts
@@ -2,8 +2,7 @@ import puppeteer, { type Browser } from 'puppeteer'
 
 export const getBrowser = async (): Promise<Browser> => {
   const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null
+    headless: 'new'
   })
   return browser
 }
